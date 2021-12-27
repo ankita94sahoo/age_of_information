a4c6d4a34c776215f714498ef73cf17da8522de9
Aksshita GUPTA <aksshita.gupta@eurecom.fr>
2021-12-27 16:44:20 +0100

    server for real time channel

---
+++ server_client/server_edited_wifi.py
@@ -0,0 +1,31 @@
+import socket
+import os
+
+HOST=''
+PORT=8665
+
+s = socket.socket()
+s.bind((HOST,PORT))
+
+s.listen(10)
+
+os.chdir("./server_files")
+
+while True:
+    sc, address = s.accept()
+
+    print(address)
+
+    f = open("Recieved_file.csv", 'wb')
+    print("Recieved all row")
+    l = sc.recv(1024)
+    while l:
+        f.write(l)
+        l = sc.recv(1024)
+
+        if not l:
+            break
+    f.close()
+    sc.close()
+    print("Copied FIle")
+s.close()
